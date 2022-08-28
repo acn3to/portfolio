f472a0c14fc37a284fcf2a9a2b73dd4f58800d16
Arnaldo Neto <costanetoow@gmail.com>
2022-08-28 20:12:24 -0300

    feat: add about component texts and animation

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import Lottie from 'react-lottie'
+import aboutAnimation from '../lotties/aboutAnimation.json'
 
 const About = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: aboutAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  }
   return (
     <div
       id="about"
@@ -14,16 +24,21 @@ const About = () => {
           </p>
           <h2 className="py-4">Quem eu sou</h2>
           <p className="py-2 text-gray-600">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut modi
-            aliquid dignissimos neque enim voluptatibus labore odio. Magnam
-            tempora libero cupiditate quae, dicta saepe sint incidunt. Ipsa
-            illum delectus esse.
-          </p>
-          <p className="py-2 text-gray-600">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut modi
-            aliquid dignissimos neque enim voluptatibus labore odio. Magnam
-            tempora libero cupiditate quae, dicta saepe sint incidunt. Ipsa
-            illum delectus esse.
+            Louco por viagens, pai, às vezes DJ, apaixonado por tecnologia e
+            entusiasta das infinitas possibilidades abertas por ela. Desde
+            criança já me aventurava no desenvolvimento de servidores
+            alternativos para jogos de RPG online. Passei a estudar o design e
+            criação de conteúdos digitais em 2011, quando me especializei em Web
+            Design e ilustração vetorial com Adobe Illustrator. Atualmente, com
+            foco em Desenvolvimento Web Front-End, me especializo em React e
+            todo seu ecossistema enquanto me graduo no curso tecnólogo em
+            Análise e Desenvolvimento de Sistemas e no curso técnico em
+            Desenvolvimento de Sistemas.
+            <p className="py-2 text-gray-600">
+              Quero desenvolver ideias e torná-las reais, com a possibilidade de
+              construir aplicações utilizáveis ​​e escaláveis ​​que façam a
+              diferença no mundo.
+            </p>
           </p>
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
@@ -31,12 +46,8 @@ const About = () => {
             </p>
           </Link>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in-out duration-300">
-          <img
-            className="rounded-xl"
-            src="https://images.unsplash.com/photo-1597239450996-ea7c2c564412?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
-            alt=""
-          />
+        <div className="w-full h-auto m-auto shadow-gray-400 flex items-center justify-center p-4 hover:scale-110 ease-in-out duration-300">
+          <Lottie options={defaultOptions} height={400} width={400} />
         </div>
       </div>
     </div>
